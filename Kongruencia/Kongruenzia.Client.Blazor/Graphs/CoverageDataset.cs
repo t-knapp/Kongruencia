270488ef67daa896f30dae00668335e86019604d
Tobias Knapp <tobias.knapp@vomatec.de>
2020-03-22 09:01:20 +0100

    Moved boilerplate chart code to separate classes.

---
+++ Kongruencia/Kongruenzia.Client.Blazor/Graphs/CoverageDataset.cs
@@ -0,0 +1,27 @@
+﻿using ChartJs.Blazor.ChartJS.Common.Enums;
+using ChartJs.Blazor.ChartJS.LineChart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kongruenzia.Client.Blazor.Graphs
+{
+    public class CoverageDataset
+    {
+        public static LineDataset<T> Create<T>(string label, string backgroundColor, string borderColor) where T : class
+        {
+            return new LineDataset<T>
+            {
+                BackgroundColor = backgroundColor,
+                BorderColor = borderColor,
+                Label = label,
+                Fill = false,
+                BorderWidth = 2,
+                PointRadius = 3,
+                PointBorderWidth = 1,
+                SteppedLine = SteppedLine.Before,
+            };
+        }
+    }
+}
---
+++ Kongruencia/Kongruenzia.Client.Blazor/Graphs/LineGraphConfig.cs
@@ -0,0 +1,73 @@
+﻿using ChartJs.Blazor.ChartJS.Common.Axes;
+using ChartJs.Blazor.ChartJS.Common.Axes.Ticks;
+using ChartJs.Blazor.ChartJS.Common.Enums;
+using ChartJs.Blazor.ChartJS.Common.Handlers;
+using ChartJs.Blazor.ChartJS.Common.Properties;
+using ChartJs.Blazor.ChartJS.Common.Time;
+using ChartJs.Blazor.ChartJS.LineChart;
+using System.Collections.Generic;
+
+namespace Kongruenzia.Client.Blazor.Graphs
+{
+    public class LineGraphConfig
+    {
+        public static LineConfig Create(string caption)
+        {
+            return new LineConfig
+            {
+                Options = new LineOptions
+                {
+                    Responsive = true,
+                    Title = new OptionsTitle
+                    {
+                        Display = true,
+                        Text = caption
+                    },
+                    Legend = new Legend
+                    {
+                        Position = Position.Right,
+                        Labels = new LegendLabelConfiguration
+                        {
+                            UsePointStyle = true
+                        }
+                    },
+                    Tooltips = new Tooltips
+                    {
+                        Mode = InteractionMode.Nearest,
+                        Intersect = false
+                    },
+                    Scales = new Scales
+                    {
+                        xAxes = new List<CartesianAxis>
+                        {
+                            new TimeAxis
+                            {
+                                Distribution = TimeDistribution.Linear,
+                                Ticks = new TimeTicks
+                                {
+                                    Source = TickSource.Data
+                                },
+                                Time = new TimeOptions
+                                {
+                                    Unit = TimeMeasurement.Millisecond,
+                                    Round = TimeMeasurement.Millisecond,
+                                    TooltipFormat = "DD.MM.YYYY HH:mm:ss",
+                                    DisplayFormats = TimeDisplayFormats.DE_CH
+                                },
+                                ScaleLabel = new ScaleLabel
+                                {
+                                    LabelString = "Time"
+                                }
+                            }
+                        },
+                    },
+                    Hover = new LineOptionsHover
+                    {
+                        Intersect = true,
+                        Mode = InteractionMode.Y
+                    }
+                }
+            };
+        }
+    }
+}
